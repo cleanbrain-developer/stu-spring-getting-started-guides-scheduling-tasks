9c1a2be29cfa65445486b67364f0e4d8bdd3cedd
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-11 20:00:40 +0900

    feat. 예제

--- src/main/java/com/example/StuSpringGettingStartedGuidesSchedulingTasksApplication.java
+++ src/main/java/com/example/StuSpringGettingStartedGuidesSchedulingTasksApplication.java
@@ -2,8 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.example.schedulingtasks.ScheduledTasks;
 
 @SpringBootApplication
+@EnableScheduling
 public class StuSpringGettingStartedGuidesSchedulingTasksApplication {
 
 	public static void main(String[] args) {
